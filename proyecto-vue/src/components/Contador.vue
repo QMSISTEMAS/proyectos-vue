b7a510e3af7d4f829fdbf8d68531a3e9be060351
Erick <qmsistemas2025@gmail.com>
2025-03-19 15:34:04 -0600

    Cambios segundo capitulo curso

---
+++ proyecto-vue/src/components/Contador.vue
@@ -0,0 +1,21 @@
+<template> <!--codigo HTML-->
+  <h1>Bienvenido a QM Manager</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Incrementar</button>
+</template>
+
+
+
+<script setup>//logica de java script
+import { ref } from 'vue';
+const contador= ref(0);
+const incrementar = () => {contador.value++}
+const decrementar = () => {contador.value--}
+</script>
+
+
+
+<style scoped>/*codigo CSS*/
+p{color: blue;}
+</style>
