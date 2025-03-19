1555b1b7321794615e1eca110d8a6a2623a1c45d
Erick <qmsistemas2025@gmail.com>
2025-03-19 15:56:06 -0600

    Cambios tercer capitulo curso

--- proyecto-vue/src/App.vue
+++ proyecto-vue/src/App.vue
@@ -1,11 +1,13 @@
 <template> <!--codigo HTML-->
   <Contador></Contador>
+  <ListaTareas></ListaTareas>
 </template>
 
 
 
 <script setup>//logica de java script
 import Contador from './components/Contador.vue';
+import ListaTareas from './components/ListaTareas.vue';
 </script>
 
 
---
+++ proyecto-vue/src/components/ListaTareas.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text" v-model="nuevatarea" @keyup.enter="addtarea">
+        <ul>
+            <li v-for="(tareas,index) in tareas" :key="index">
+                {{ tareas }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+
+<script setup>
+import {ref} from 'vue';
+const tareas =ref([]);
+const nuevatarea =ref('');
+const addtarea = () =>{
+    tareas.value.push(nuevatarea.value)
+    nuevatarea.value='';
+
+} 
+</script>
+
+
+
+<style>
+
+</style>
